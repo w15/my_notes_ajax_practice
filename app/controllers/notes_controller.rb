261138ed173977c95e1a1e7935a3ad8ff9c849c8
Raghu Betina <rbetina@github.com>
2015-03-13 14:50:30 -0500

    Moves to partials

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -30,6 +30,7 @@ class NotesController < ApplicationController
       if @note.save
         format.html { redirect_to :back, notice: 'Note was successfully created.' }
         format.json { render :show, status: :created, location: @note }
+        format.js
       else
         format.html { render :new }
         format.json { render json: @note.errors, status: :unprocessable_entity }
@@ -58,6 +59,7 @@ class NotesController < ApplicationController
     respond_to do |format|
       format.html { redirect_to notes_url, notice: 'Note was successfully destroyed.' }
       format.json { head :no_content }
+      format.js
     end
   end
 
