4da005a55384880aa083887fc4975ee8b8b72c94
Raghu Betina <rbetina@github.com>
2015-03-13 14:54:45 -0500

    Ajaxified Edit

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -19,6 +19,10 @@ class NotesController < ApplicationController
 
   # GET /notes/1/edit
   def edit
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   # POST /notes
